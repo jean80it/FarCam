fe12cb92579f5295401c7bda7e3f0efd797fba8f
Giancarlo Todone <giancarlotodone@yahoo.it>
2013-03-26 19:12:42 +0100

    Applied copyright notice / license

--- src/com/jean/FarCam/FarCamActivity.java
+++ src/com/jean/FarCam/FarCamActivity.java
@@ -1,3 +1,17 @@
+//    Copyright 2013 Giancarlo Todone
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
 package com.jean.FarCam;
 
 import android.app.Activity;
@@ -13,11 +27,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceView;
-import com.jean.FarCam.R;
 
 public class FarCamActivity extends Activity {
 
-    private static final String TAG = "remoteCamera";
+    private static final String TAG = "FarCam";
 
     private ServerThread _serverThread = null;
 
